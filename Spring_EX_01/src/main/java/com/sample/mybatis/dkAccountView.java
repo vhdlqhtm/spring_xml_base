edac3fdf0001af56a82e684f55590a9f18e3a85d
미정 <@DK>
2017-11-16 10:51:59 +0900

    셋팅파일

--- Spring_EX_01/src/main/java/com/sample/mybatis/dkAccountView.java
+++ Spring_EX_01/src/main/java/com/sample/mybatis/dkAccountView.java
@@ -28,20 +28,26 @@ public class dkAccountView {
 	//@RequestMapping(value = "/dk/accountRegister.do", method = RequestMethod.POST)
 	@ResponseBody
 	public String accountRegister(@RequestBody MemberVO vo) throws Exception{
-		//json 형식으로 ajax하기위해선 jackson librery 필요하고 해당 파일은 di로 추가해주면됨
-		//Ajax 에서 보낸값을 리턴하기위해 @ResponseBody를 사용, @RequestBody는 ajax에서 던진 값을 받기위해 사용
+		//json �삎�떇�쑝濡� ajax�븯湲곗쐞�빐�꽑 jackson librery �븘�슂�븯怨� �빐�떦 �뙆�씪�� di濡� 異붽��빐二쇰㈃�맖
+		//Ajax �뿉�꽌 蹂대궦媛믪쓣 由ы꽩�븯湲곗쐞�빐 @ResponseBody瑜� �궗�슜, @RequestBody�뒗 ajax�뿉�꽌 �뜕吏� 媛믪쓣 諛쏄린�쐞�빐 �궗�슜
 		
 		service.accountRegister(vo);
 		
-		String aa = "성공";
+		String aa = "�꽦怨�";
 		return aa;
 	}
 	
-	//회원가입 페이지 이동
+	//�쉶�썝媛��엯 �럹�씠吏� �씠�룞
 		@GetMapping(value = "/dk/login.do")
 		public ModelAndView userJoinView() {
-			logger.info("로그인페이지");
+			logger.info("濡쒓렇�씤�럹�씠吏�");
 			ModelAndView mav = new ModelAndView("main");
+			return mav;
+		}
+		
+		@GetMapping(value = "/test.do")
+		public ModelAndView test() {
+			ModelAndView mav = new ModelAndView("test");
 			return mav;
 		}
 	
